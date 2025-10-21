1599ac3616ede8eb9dbac568952ad51d172a7a50
ngtuyn <ngct4532@gmail.com>
2025-10-21 09:04:35 +0700

    update layout & myorder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
   const handleFinishOnboarding = () => {
     setShowOnboarding(false);
     setTimeout(() => {
-      router.replace('/auth/_welcome');
+      router.replace('/');
     }, 50);
   };
 
